f77c311ca5eab5ae07957a317dbb8ba2642d97ff
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-10-02 14:05:00 +0200

    Parsing logic set up

--- OOTServer/PlaintextServer.cs
+++ OOTServer/PlaintextServer.cs
@@ -35,31 +35,39 @@ namespace OOTServer
             Console.WriteLine($"New client: {socket.Client.RemoteEndPoint}");
             StreamReader reader = new(socket.GetStream());
             StreamWriter writer = new(socket.GetStream());
-            writer.WriteLine("Hiya :D please input your command. You can choose between:");
-            writer.WriteLine("\tRandom");
-            writer.WriteLine("\tAdd");
-            writer.WriteLine("\tSubtract");
-            writer.Flush();
+            //writer.WriteLine("Hiya :D please input your command. You can choose between:");
+            //writer.WriteLine("\tRandom");
+            //writer.WriteLine("\tAdd");
+            //writer.WriteLine("\tSubtract");
+            //writer.Flush();
             string command = reader.ReadLine();
             switch (command)
             {
                 case "Random":
-                    writer.WriteLine("Random ain't implemented yet, but I did recognize it");
+                    writer.WriteLine("Random recognized");
                     writer.Flush();
+                    reader.DiscardBufferedData();
                     break;
                 case "Add":
-                    writer.WriteLine("Add ain't implemented yet, but I did recognize it");
+                    writer.WriteLine("Add recognized");
                     writer.Flush();
+                    reader.DiscardBufferedData();
                     break;
                 case "Subtract":
-                    writer.WriteLine("Subtract ain't implemented yet, but I did recognize it");
+                    writer.WriteLine("Subtract recognized");
                     writer.Flush();
+                    reader.DiscardBufferedData();
                     break;
                 default:
-                    writer.WriteLine($"Sorry, didn't recognize that. What I got was: {command}");
+                    writer.WriteLine($"Not recognized");
                     writer.Flush();
+                    reader.DiscardBufferedData();
                     break;
             }
+            //reader = new(socket.GetStream());
+            //writer = new(socket.GetStream());
+            string numbers = reader.ReadLine();
+            Console.WriteLine($"Numbers received: {numbers}");
         }
     }
 }
